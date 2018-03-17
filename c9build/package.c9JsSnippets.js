38d73fa58214add4564323e4f7f28b0b9524127f
kristie@anchorlabs.com <kristie@anchorlabs.com>
2018-03-17 00:46:47 +0000

    Use the snippets directory

--- c9build/package.c9JsSnippets.js
+++ c9build/package.c9JsSnippets.js
@@ -1,6 +1,39 @@
 define("plugins/c9JsSnippets/package.c9JsSnippets", [], {
     "name": "c9JsSnippets",
     "c9": {
-        "plugins": []
+        "plugins": [
+            {
+                "packagePath": "plugins/c9JsSnippets/__static__"
+            }
+        ]
+    }
+});
+
+define("plugins/c9JsSnippets/__static__",[], function(require, exports, module) {
+    main.consumes = [
+        "Plugin", "plugin.debug"
+    ];
+    main.provides = [];
+    return main;
+    function main(options, imports, register) {
+        var debug = imports["plugin.debug"];
+        var Plugin = imports.Plugin;
+        var plugin = new Plugin();
+        plugin.version = "undefined";
+        plugin.on("load", function load() {
+            [
+                {
+                    "type": "snippets",
+                    "filename": "javascript.snippets",
+                    "data": "# scope: javascript\nsnippet test\n\tif (${1:true}) {\n\t\t${2}\n\t}\n\t$0\n\t\nsnippet pleaseWork\n\tif (${1:true}) {\n\t\t${2}\n\t} else {\n\t\t${0}\n\t}\n"
+                }
+            ].forEach(function(x) {
+                debug.addStaticPlugin(x.type, "c9JsSnippets", x.filename, x.data, plugin);
+            });
+        });
+        
+        plugin.load("c9JsSnippets.bundle");
+        
+        register(null, {});
     }
 });
